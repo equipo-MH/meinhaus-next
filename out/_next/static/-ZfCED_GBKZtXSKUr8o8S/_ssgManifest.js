117ad24c8c838d2418c562f86caa972a0eef929e
Mariano Serdoch <mariano@meinhaus.com.ar>
2026-04-07 21:37:33 -0300

    v19: fix use client directive

--- out/_next/static/-ZfCED_GBKZtXSKUr8o8S/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Farticulos\u002F[slug]","\u002Fcapas\u002F[tipo]","\u002Fpaises\u002F[iso]","\u002Fregiones\u002F[slug]","\u002Fsistemas\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
